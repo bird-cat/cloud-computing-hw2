9674aab4aaf90073be3d1a65f2df72fb56303307
bird-cat <51794376+bird-cat@users.noreply.github.com>
2020-10-18 17:12:42 +0800

    Update insert_personal_info.py

--- insert_personal_info.py
+++ insert_personal_info.py
@@ -2,19 +2,19 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-        dynamodb = boto3.resource('dynamodb')
-            client = boto3.client('dynamodb')
-                
-                    table = dynamodb.Table('personal_info')
-                        table.put_item(
-                                        Item = {
-                                                        'name': event['name'],
-                                                                    'student id': event['student id'],
-                                                                                'birthday': event['birthday'],
-                                                                                            'cellphone': event['cellphone']
-                                                                                                    }    
-                                            )
-                            return {
-                                            'statusCode': 200,
-                                                    'body': json.dumps('Successfully insert an info!')
-                                                        }
+    dynamodb = boto3.resource('dynamodb')
+    client = boto3.client('dynamodb')
+    
+    table = dynamodb.Table('personal_info')
+    table.put_item(
+        Item = {
+            'name': event['name'],
+            'student id': event['student id'],
+            'birthday': event['birthday'],
+            'cellphone': event['cellphone']
+        }    
+    )
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successfully insert an info!')
+    }
